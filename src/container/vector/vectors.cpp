c68c9f5edbb35821841a55190d85ee4ec2486b1a
dietoad <dietoad@gmail.com>
2022-04-30 14:13:40 +0800

    update accumulate

--- src/container/vector/vectors.cpp
+++ src/container/vector/vectors.cpp
@@ -3,6 +3,8 @@
 //
 
 #include<benchmark/benchmark.h>
+#include<algorithm>
+#include<numeric>
 #include "SmallVector.h"
 #include "vector.hpp"
 #include <vector>
@@ -36,6 +38,37 @@ BENCHMARK_TEMPLATE(BenchReserve,std::vector<int>)->Range(2,1024);
 BENCHMARK_TEMPLATE(BenchReserve,llvm::SmallVector<int,1024>)->Range(2,1024);
 BENCHMARK_TEMPLATE(BenchReserve,llvm_vecsmall::SmallVector<int,1024>)->Range(2,1024);
 
+
+template<typename V>
+static void BenchAccumulate(benchmark::State &state) {
+    V v;
+    v.reserve(state.range(0));
+    for (auto i = 0; i < state.range(0); i++) {
+        v.push_back(i);
+    }
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(std::accumulate(v.begin(),v.end(),0));
+    }
+}
+BENCHMARK_TEMPLATE(BenchAccumulate,std::vector<int>)->Range(2,1024);
+BENCHMARK_TEMPLATE(BenchAccumulate,llvm::SmallVector<int,1024>)->Range(2,1024);
+BENCHMARK_TEMPLATE(BenchAccumulate,llvm_vecsmall::SmallVector<int,1024>)->Range(2,1024);
+
+template<typename V>
+static void BenchAccumulateReverse(benchmark::State &state) {
+    V v;
+    v.reserve(state.range(0));
+    for (auto i = 0; i < state.range(0); i++) {
+        v.push_back(i);
+    }
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(std::accumulate(v.rbegin(),v.rend(),0));
+    }
+}
+
+BENCHMARK_TEMPLATE(BenchAccumulateReverse,std::vector<int>)->Range(2,1024);
+BENCHMARK_TEMPLATE(BenchAccumulateReverse,llvm::SmallVector<int,1024>)->Range(2,1024);
+BENCHMARK_TEMPLATE(BenchAccumulateReverse,llvm_vecsmall::SmallVector<int,1024>)->Range(2,1024);
 int main(int argc, char **argv) {
     benchmark::Initialize(&argc, argv);
     benchmark::RunSpecifiedBenchmarks();
