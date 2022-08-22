167571854ce897f1257f9063ce9238266311501e
zhangwenhao <dietoad@gmail.com>
2022-08-22 21:19:42 +0800

    add fbstring example

--- src/coroutine/fiber/folly/sorted.cpp
+++ src/coroutine/fiber/folly/sorted.cpp
@@ -4,8 +4,11 @@
 
 #include "SortedVector.h"
 #include <benchmark/benchmark.h>
-#include <map>
+#include <folly/FBString.h>
 #include <folly/sorted_vector_types.h>
+#include <folly/container/heap_vector_types.h>
+#include <map>
+
 static unsigned long xorshf96()
 { /* A George Marsaglia generator, period 2^96-1 */
     static unsigned long x = 103456789, y = 362436069, z = 521088629;
@@ -43,15 +46,15 @@ template <typename V> static void BenchInsert(benchmark::State &state)
         for (auto i = 0; i < state.range(0); i++)
         {
             auto r = _random();
-            v[r]=r;
+            v[r]   = r;
         }
     }
 }
 
-BENCHMARK_TEMPLATE(BenchInsert, sorted_vector_map<uint32_t,uint32_t>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchInsert, sorted_vector_map<uint32_t, uint32_t>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchInsert, folly::sorted_vector_map<uint32_t, uint32_t>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchInsert, folly::heap_vector_map<uint32_t,uint32_t>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchInsert, std::map<uint32_t, uint32_t>)->Range(1, 1 << 10);
-
 
 template <typename V> static void BenchFind(benchmark::State &state)
 {
@@ -68,8 +71,9 @@ template <typename V> static void BenchFind(benchmark::State &state)
     }
 }
 
-BENCHMARK_TEMPLATE(BenchFind, sorted_vector_map<uint64_t,Pod>)->Range(1, 1 << 10);
-BENCHMARK_TEMPLATE(BenchFind, folly::sorted_vector_map<uint32_t, uint32_t>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchFind, sorted_vector_map<uint64_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchFind, folly::sorted_vector_map<uint32_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchFind, folly::heap_vector_map<uint32_t,Pod>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchFind, std::map<uint64_t, Pod>)->Range(1, 1 << 10);
 
 template <typename V> static void BenchRange(benchmark::State &state)
@@ -91,8 +95,9 @@ template <typename V> static void BenchRange(benchmark::State &state)
     benchmark::DoNotOptimize(sum);
 }
 
-BENCHMARK_TEMPLATE(BenchRange, sorted_vector_map<uint64_t,Pod>)->Range(1, 1 << 10);
-BENCHMARK_TEMPLATE(BenchRange, folly::sorted_vector_map<uint32_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchRange, sorted_vector_map<uint64_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchRange, folly::sorted_vector_map<uint64_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchRange, folly::heap_vector_map<uint32_t,Pod>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchRange, std::map<uint64_t, Pod>)->Range(1, 1 << 10);
 
 template <typename V> static void BenchErase(benchmark::State &state)
@@ -114,9 +119,28 @@ template <typename V> static void BenchErase(benchmark::State &state)
     }
 }
 
-BENCHMARK_TEMPLATE(BenchErase, sorted_vector_map<uint64_t,Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchErase, sorted_vector_map<uint64_t, Pod>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchErase, folly::sorted_vector_map<uint32_t, Pod>)->Range(1, 1 << 10);
+BENCHMARK_TEMPLATE(BenchErase, folly::heap_vector_map<uint32_t,Pod>)->Range(1, 1 << 10);
 BENCHMARK_TEMPLATE(BenchErase, std::map<uint64_t, Pod>)->Range(1, 1 << 10);
+
+template <typename V> static void BenchStringFind(benchmark::State &state)
+{
+    V s = "";
+    for (auto i = 0; i < state.range(); i++)
+    {
+        s += std::to_string(_random() % UINT64_MAX);
+    }
+    auto needle = std::to_string(_random() % UINT32_MAX);
+
+    for (auto _ : state)
+    {
+        benchmark::DoNotOptimize(s.find(needle));
+    }
+}
+
+BENCHMARK_TEMPLATE(BenchStringFind, std::string)->Range(128, 1 << 16);
+BENCHMARK_TEMPLATE(BenchStringFind, folly::fbstring)->Range(128, 1 << 16);
 
 int main(int argc, char **argv)
 {
